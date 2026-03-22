79fda1f089f4c3def1e617f5368af3a3143069ca
RevoSucks <projectrevotpp@hotmail.com>
2026-03-22 18:24:10 -0400

    v1.0.0, initial release

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -58,7 +58,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "0.9.9";
+const std::string version_string = "1.0.0";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
